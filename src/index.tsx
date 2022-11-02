5b74e77a1812610b5d12f07777439470c204ecda
seon <seonsy44@gmail.com>
2022-11-02 11:21:14 +0900

    feat: 스타일 관련 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
+import colors from './styles/theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <ThemeProvider theme={colors}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+const colors = {
+  black: '#000000',
+  gray: '#D9D9D9',
+  blue: '#0094FF',
+};
+
+export default colors;
