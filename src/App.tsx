b1b6e24b1ad620cc03d6bab222ef37941158cb6a
seon <seonsy44@gmail.com>
2022-11-02 11:25:13 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div />;
+  return (
+    <Routes>
+      <Route path="/vehicles" element={<div>vehicles</div>} />
+      <Route path="/vehicles/:id" element={<div>vehicles detail</div>} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import colors from './styles/theme';
@@ -10,7 +11,9 @@ root.render(
   <React.StrictMode>
     <GlobalStyle />
     <ThemeProvider theme={colors}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
