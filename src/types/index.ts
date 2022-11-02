27bbf06631aad56baa6ffe84ebe0e16cb5347317
seon <seonsy44@gmail.com>
2022-11-02 16:01:33 +0900

    feat: 차량 type 지정

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export type VehicleBasic = {
+  id: number;
+  startDate: Date;
+  createdAt: Date;
+  amount: number;
+  attribute: VehicleAttribute;
+  insurance: VehicleInsurance[];
+  additionalProducts: VehicleAdditionalProducts[];
+};
+
+export type VehicleAttribute = {
+  brand: string;
+  name: string;
+  segment: VehicleSegment;
+  fuelType: VehicleFuelType;
+  imageUrl: string;
+};
+
+export type VehicleInsurance = {
+  name: string;
+  description: string;
+};
+
+export type VehicleAdditionalProducts = {
+  name: string;
+  amount: number;
+};
+
+export type VehicleSegment = 'C' | 'D' | 'E' | 'SUV';
+
+export type VehicleFuelType = 'gasoline' | 'hybrid' | 'ev';
