706265a8edc478763a86ca03e9486ccc1d0624db
seon <seonsy44@gmail.com>
2022-11-03 14:54:16 +0900

    feat: 루트 path 관련 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import VehicleDetail from './pages/VehicleDetail';
 import VehicleList from './pages/VehicleList';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/vehicles" />} />
       <Route path="/vehicles" element={<VehicleList />} />
       <Route path="/vehicles/:id" element={<VehicleDetail />} />
     </Routes>
