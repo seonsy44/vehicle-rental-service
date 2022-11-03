ec0fda1e2922887a1ec320f6cef9684b4a3e84d0
seon <seonsy44@gmail.com>
2022-11-03 14:47:38 +0900

    feat: VehicleDetail 페이지 구현

---
+++ src/pages/VehicleDetail/index.tsx
@@ -0,0 +1,66 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+import Container from '../../components/Container';
+import Header from '../../components/Header';
+import SectionList from '../../components/SectionList';
+import { flexBox } from '../../styles/mixin';
+import { VehicleBasic } from '../../types';
+import { getVehicleDetailSections } from '../../utils/utils';
+import SectionHeader from './SectionHeader';
+import SectionItem from './SectionItem';
+
+function VehicleDetail() {
+  const {
+    state: { vehicle },
+  }: { state: { vehicle: VehicleBasic } } = useLocation();
+
+  if (!vehicle) return <Navigate to="/vehicles" />;
+
+  const sections = getVehicleDetailSections(vehicle);
+  const renderSectionHeader = (section: { title: string }) => <SectionHeader section={section} />;
+  const renderSectionItem = (item: { item?: string; content: string }) => <SectionItem item={item} />;
+
+  return (
+    <Container customStyle={ContainerStyle}>
+      <Header title="차량상세" hasBackButton />
+      <Image alt={vehicle.attribute.name} src={vehicle.attribute.imageUrl} />
+      <Title>
+        <div>{vehicle.attribute.brand}</div>
+        <div>{vehicle.attribute.name}</div>
+      </Title>
+      <SectionItem item={{ content: `월 ${vehicle.amount.toLocaleString()} 원` }} />
+      <SectionList sections={sections} renderSectionHeader={renderSectionHeader} renderItem={renderSectionItem} />
+    </Container>
+  );
+}
+
+export default VehicleDetail;
+
+const ContainerStyle = css`
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 205px;
+`;
+
+const Title = styled.div`
+  ${flexBox('column', 'center', 'flex-start')};
+  width: 100%;
+  height: 92px;
+  padding: 0 ${({ theme }) => theme.paddingHorizontal};
+  font-weight: 700;
+
+  > div:first-child {
+    font-size: 20px;
+    line-height: 24px;
+  }
+  > div:last-child {
+    font-size: 24px;
+    line-height: 29px;
+  }
+`;
