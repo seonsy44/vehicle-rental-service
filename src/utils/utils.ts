f35338760a945814dca5136f8eb2a2ef793ff288
seon <seonsy44@gmail.com>
2022-11-03 11:44:35 +0900

    feat: section list에 필요한 util 구현

---
+++ src/utils/utils.ts
@@ -0,0 +1,41 @@
+import { VehicleBasic } from '../types';
+import { day, vehicleFuelType, vehicleSegment } from './const';
+
+export const parseDate = (date: Date) => {
+  const newDate = new Date(date);
+  return `${newDate.getMonth() + 1}월 ${newDate.getDate()}일 (${day[newDate.getDay()]})`;
+};
+
+export const getVehicleDetailSections = (vehicle: VehicleBasic) => [
+  {
+    title: '차량정보',
+    data: [
+      {
+        item: '차종',
+        content: vehicleSegment[vehicle.attribute.segment],
+      },
+      {
+        item: '연료',
+        content: vehicleFuelType[vehicle.attribute.fuelType],
+      },
+      {
+        item: '이용 가능일',
+        content: `${parseDate(vehicle.startDate)} 부터`,
+      },
+    ],
+  },
+  {
+    title: '보험',
+    data: vehicle.insurance.map((i) => ({
+      item: i.name,
+      content: i.description,
+    })),
+  },
+  {
+    title: '추가상품',
+    data: vehicle.additionalProducts.map((p) => ({
+      item: p.name,
+      content: `월 ${p.amount.toLocaleString()} 원`,
+    })),
+  },
+];
